8b98b97de636ffbe3f129304b6262f1a40824f77
IguanoVP <122044076@upq.edu.mx>
2024-11-01 17:08:11 -0600

    plantilla

--- sustainityPI/resources/views/CrearCuenta.blade.php
+++ sustainityPI/resources/views/CrearCuenta.blade.php
@@ -1,30 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Sustainity</title>
-    <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
-    @vite(['resources/css/login.css'])
-    @vite('resources/js/script.js')
-</head>
-<body>
-<header class="navbar">
-    <div class="navbar-left">
-        <a href="#">
-            <img src="{{ asset('img/DevPlay logo.png') }}" alt="Logo" class="logo-image">
-        </a>
-    </div>
-    <nav class="navbar-center nav-links">
-    <a href="{{ route('rutaInicio') }}">Inicio</a>
-        <a href="/donar">Donativos</a>
-        <a href="{{ route('rutaNosotros')}}">Nosotros</a>
-    </nav>
-    <div class="navbar-right auth-buttons">
-        <button class="login-btn" onclick="window.location.href='{{ route('rutaLogin') }}'">Iniciar Sesión</button>
-        <button class="news-btn" onclick="window.location.href='{{ route('rutaNoticias') }}'">Noticias</button>
-    </div>
-</header>
+@extends('layouts.Plantilla')
+@section('Login')
+@section('contenido')
 
     @if (session('message'))
 <div id="alerta_tiempo" class="alert" style="width: 100%; padding: 15px 0; position: fixed; top: 80px; left: 0; z-index: 1000;">
@@ -67,15 +43,3 @@
         </div>
         <img src="{{ asset('img/character.png') }}" alt="Game Character" class="character-image">
     </div>
-
-    <footer class="footer">
-        <div class="footer-content">
-            <a href="#">Política de Privacidad</a> | 
-            <a href="#">Términos y Condiciones</a> | 
-            <a href="#">Contacto</a>
-        </div>
-        <p>&copy; 2024 Sustainity. Todos los derechos reservados.</p>
-    </footer>
-    
-</body>
-</html>
